f4d4ba1dc2fd1bd2484d98e5281e761fdcb405bb
221V <221v92@gmail.com>
2019-09-02 17:58:27 +0300

    prepare html v0.5

--- pre/pre.erl
+++ pre/pre.erl
@@ -37,8 +37,11 @@ show(#xmlElement{name=nav,content=C}, SP, L, _, RA) ->
   [ nav_child(1, C, "  " ++ SP, L, []) |RA];
 show(#xmlElement{name=header,content=C}, SP, L, _In, RA) ->
   [ ["<header>", "\n", child(C, "  " ++ SP, L, in_header, []), "</header>", "\n"] |RA];
-show(#xmlElement{name=a,attributes=[#xmlAttribute{name=href, value=AV}|_],content=C}, _SP, L, {in_p,_}, RA) ->
+show(#xmlElement{name=a,attributes=[#xmlAttribute{name=href, value=AV}|_],content=C}, _SP, L, In, RA)
+when In =:= in_p; In =:= in_b ->
   [ ["<a href=\"", AV, "\">", child(C, "", L, in_a, []), "</a>"] |RA];
+show(#xmlElement{name=a,attributes=[#xmlAttribute{name=href, value=AV}|_],content=C}, SP, L, {in_p,last}, RA) ->
+  [ [SP, "  ", "<a href=\"", AV, "\">", child(C, "", L, in_a, []), "</a>"] |RA];
 show(#xmlElement{name=a,attributes=[#xmlAttribute{name=href, value=AV}|_],content=C}, SP, L, _In, RA) ->
   [ [SP, "<a href=\"", AV, "\">", child(C, "", L, in_a, []), "</a>", "\n"] |RA];
 show(#xmlElement{name=h1,content=C}, SP, L, _In, RA) ->
@@ -48,7 +51,9 @@ show(#xmlElement{name=h3,content=C}, SP, L, _In, RA) ->
 show(#xmlElement{name=h4,content=C}, SP, L, _In, RA) ->
   SP2 = "  " ++ SP,
   [ [SP, "<figure>", "\n", SP2, "<figcaption></figcaption>", "\n",
-     SP2, "<code>", child(C, "", L, in_h3, []), SP2, "</code>", "\n", SP, "</figure>", "\n"] |RA];
+     SP2, "<code>", "\n", child(C, "", L, in_h3, []), "\n", SP2, "</code>", "\n", SP, "</figure>", "\n"] |RA];
+show(#xmlElement{name=b,content=C}, SP, L, {in_p, last}, RA) ->
+  [ [SP, "  ", "<b>", child(C, "", L, in_b, []), "</b>"] |RA];
 show(#xmlElement{name=b,content=C}, _SP, L, _In, RA) ->
   [ ["<b>", child(C, "", L, in_b, []), "</b>"] |RA];
 show(#xmlElement{name=aside,content=C}, SP, L, In, RA) ->
@@ -64,15 +69,23 @@ show(#xmlElement{name=main,content=C}, SP, L, In, RA) ->
 show(#xmlElement{name=article,content=C}, SP, L, In, RA) ->
   [ [SP, "<article>", "\n", child(C, SP, L, In, []), "  " ++ SP, "</article>", "\n"] |RA];
 show(#xmlElement{name=section,content=C}, SP, L, In, RA) ->
-  [ [SP, "<section>", "\n", child(C, "  " ++ SP, L, In, []), SP, "</section>", "\n"] |RA];
-show(#xmlElement{name=p,content=C}, SP, L, _In, RA) ->
-  [ [SP, "<p>", child(C, SP, L, {in_p,1}, []), "</p>", "\n"] |RA];
+  In2 = case check(C, false) of
+    {true, last} -> last;
+    _ -> In
+  end,
+  [ [SP, "<section>", "\n", child(C, "  " ++ SP, L, In2, []), SP, "</section>", "\n"] |RA];
+show(#xmlElement{name=p,content=C}, SP, L, In, RA) ->
+  In2 = case In of
+    last -> {in_p, last};
+    _ -> in_p
+  end,
+  [ [SP, "<p>", child(C, SP, L, In2, []), "</p>", "\n"] |RA];
 show(#xmlElement{name=figure,content=C}, SP, L, In, RA) ->
   [ [SP, "<figure>", "\n", child(C, "  " ++ SP, L, In, []), SP, "</figure>", "\n"] |RA];
 show(#xmlElement{name=figcaption,content=C}, SP, L, _In, RA) ->
   [ [SP, "<figcaption>", child(C, SP, L, in_figcaption, []), "</figcaption>", "\n"] |RA];
 show(#xmlElement{name=code,content=C}, SP, L, _In, RA) ->
-  [ [SP, "<code>", child(C, SP, L, in_code, []), "</code>", "\n"] |RA];
+  [ [SP, "<code>", "\n", child(C, SP, L, in_code, []), "\n", SP, "</code>", "\n"] |RA];
 show(#xmlElement{name=img,attributes=[#xmlAttribute{name=src, value=SV}|_]}, SP, _L, in_a, RA) ->
   [ [SP, "<img src=\"", SV, "\" \>"] |RA];
 show(#xmlElement{name=img,attributes=[#xmlAttribute{name=src, value=SV}|_]}, SP, _L, _In, RA) ->
@@ -85,30 +98,41 @@ show(#xmlElement{name=footer,content=C}, SP, L, _In, RA) ->
   [ [SP, "<footer>", child(C, "", L, in_footer, []), "</footer>", "\n"] |RA];
 show(#xmlElement{name=br}, _SP, _L, _In, RA) ->
   [ "<br \>" |RA];
-show(#xmlText{value=V}, SP, _L, {in_p,1}, RA) ->
-  V2 = trim(V),
-  Last = hd(lists:reverse(V)), % "\n" == 10, "." == 46, " " == 32
-  case V2 of
-    "" -> RA;
-    "." -> [ V2 |RA];
-    "," -> [ V2 |RA];
-    _ -> if Last =:= 10 -> [ [V2, "\n"] |RA]; Last =:= 32 -> [ [V2, " "] |RA]; true -> [ V2 |RA] end
+show(#xmlText{value="\n"}, _SP, _L, In, RA) when In =:= in_p; In =:= {in_p,last} ->
+  [ "\n" |RA];
+show(#xmlText{value="\n"}, _SP, _L, In, RA) when In =:= in_b ->
+  RA;
+show(#xmlText{value=V,pos=1}, SP, _L, In, RA) when In =:= in_p; In =:= {in_p,last} ->
+  N = nlc(V,0),
+  if N > 1 ->
+      V01 = string:tokens(V,"\n"),
+      V02 = txtp(hd(V01), SP, {f, n, f}),
+      V03 = txtp(tl(V01), SP, {f, n}, []),
+      [ [V02, V03] |RA];
+    true ->
+      
+      V02 = txtp(V, SP, {f, s}),
+      [V02|RA]
   end;
-show(#xmlText{value=V}, SP, _L, {in_p,_}, RA) ->
-  V2 = trim(V),
-  Last = hd(lists:reverse(V)), % "\n" == 10, "." == 46, " " == 32
-  case V2 of
-    "" -> RA;
-    "." -> if V =:= ".\n" -> [ V |RA];true -> [ V2 |RA] end;
-    "," -> if V =:= ",\n" -> [ V |RA];true -> [ V2 |RA] end;
-    _ -> if Last =:= 10 -> [ ["\n", SP, V2] |RA]; Last =:= 32 -> [ ["\n", SP, V2, " "] |RA]; true -> ["\n", SP, V2 |RA] end
+show(#xmlText{value=V,pos=_Z}, SP, _L, In, RA) when In =:= in_p; In =:= {in_p,last} ->
+  %io:format("Z: ~p~n",[Z]),
+  N = nlc(V,0),
+  if N > 1 ->
+      V01 = string:tokens(V,"\n"),
+      V02 = txtp(hd(V01), SP, {n, n}),
+      V03 = txtp(tl(V01), SP, {n, n}, []),
+      [ [V02, V03] |RA];
+    true->
+    
+      V02 = txtp(V, SP, {n, s}),
+      [V02|RA]
   end;
 show(#xmlText{value=V}, _SP, _L, In, RA)
 when In =:= in_a; In =:= in_h1; In =:= in_h3; In =:= in_sub; In =:= in_sup; In =:= in_b; In =:= in_figcaption ->
   [ V |RA];
-show(#xmlText{value=V}, SP, _L, in_code, RA) ->
+show(#xmlText{value=V}, _SP, _L, in_code, RA) ->
   [ esc_m(V, []) |RA];
-show(#xmlText{value=V}, SP, _L, In, RA) when In =:= in_header; In =:= in_footer ->
+show(#xmlText{value=V}, _SP, _L, In, RA) when In =:= in_header; In =:= in_footer ->
   V2 = trim(V),
   case V2 of
     "" -> RA;
@@ -121,6 +145,7 @@ show(#xmlText{value=V}, SP, _L, _In, RA) ->
     "." -> [ V2 |RA];
     "," -> [ V2 |RA];
     _ -> [ [ SP, V2 ] |RA]
+    %_ -> [ [ "  ", V2 ] |RA]
   end;
 %show(#xmlElement{name=_, content=C}, SP, L, In, RA) ->
 %  child(C, "  " ++ SP, L, In, RA);
@@ -135,7 +160,6 @@ show(Z, _, _, In, RA) ->
 
 
 child([], _, _, _, RA) -> lists:reverse(RA);
-child([Node | MoreNodes], SP, L, {in_p,1}, RA) -> child(MoreNodes, SP, L, {in_p,2}, show(Node, SP, L, {in_p,1}, RA));
 child([Node | MoreNodes], SP, L, In, RA) -> child(MoreNodes, SP, L, In, show(Node, SP, L, In, RA)).
 
 
@@ -221,6 +245,86 @@ esc_m([38, 108, 116, 59|T], Acc) -> esc_m(T, [$;, $0, $6, $#, $&|Acc]); % "&lt;"
 esc_m([62|T], Acc) -> esc_m(T, [$;, $2, $6, $#, $&|Acc]); % ">" -> "&#62;"
 esc_m([38, 103, 116, 59|T], Acc) -> esc_m(T, [$;, $2, $6, $#, $&|Acc]); % "&gt;" -> "&#62;"
 esc_m([H|T], Acc) -> esc_m(T, [H|Acc]).
+
+nlc([], N) -> N;
+nlc([10|T], N) -> nlc(T, N + 1); % "\n" == 10
+nlc([_|T], N) -> nlc(T, N).
+
+% Type = {f, s} | {f, n} | {f, n, f} | {n, s} | {n, n} (first_in_p, not first, single_row, not single, first_in_p_and_first_in_not_single_row)
+txtp(V, _SP, Type) when Type =:= {f,s}; Type =:= {f, n, f} ->
+  V2 = trim(V),
+  Last = hd(lists:reverse(V)), % "\n" == 10, "." == 46, " " == 32, "—" == 8212
+  case V2 of
+    "" -> "";
+    "." -> V2;
+    "," -> V2;
+    _ ->
+      if Type =:= {f,s}, Last =:= 10 -> [V2, "\n"];
+        Type =:= {f,s}, Last =:= 32 -> [V2, " "];
+        Type =:= {f,n,f}, Last =:= 32 -> [V2, " "];
+        Type =:= {f,s} -> V2;
+        true -> [V2, "\n"]
+      end
+  end;
+txtp(V, SP, {f,n}) ->
+  V2 = trim(V),
+  Last = hd(lists:reverse(V)), % "\n" == 10, "." == 46, " " == 32, "—" == 8212
+  case V2 of
+    "" -> "";
+    "." -> V2;
+    "," -> V2;
+    _ ->
+      if Last =:= 32 -> [SP, "  ", V2, " "];
+        true -> [SP, "  ", V2, "\n"]
+      end
+  end;
+txtp(V, SP, {n, s}) ->
+  V2 = trim(V),
+  Last = hd(lists:reverse(V)), % "\n" == 10, "." == 46, " " == 32, "—" == 8212
+  if V =:= ",\n"; V =:= ".\n"; V =:= " ."; V =:= ", " -> V;
+    V2 =:= "."; V2 =:= "," -> V2;
+    V2 =:= "" -> "";
+    true ->
+      B = if [hd(V2)] =:= "—" -> " "; hd(V) =:= 10 -> ["\n", "  ", SP]; true -> "" end,
+      if Last =:= 10 -> [B, V2, "\n"]; Last =:= 32 -> [B, V2, " "]; true -> [B, V2] end
+  end;
+txtp(V, SP, {n, n}) ->
+  V2 = trim(V),
+  Last = hd(lists:reverse(V)), % "\n" == 10, "." == 46, " " == 32, "—" == 8212
+  if V =:= ","; V =:= "." -> V;
+    [hd(V)] =:= "," -> V2;
+    V =:= " ."; V =:= ", " -> ["\n", SP, "  ", V];
+    V2 =:= "."; V2 =:= "," -> [ V2, "\n"];
+    V2 =:= "" -> ["\n", SP, "  "];
+    true ->
+      B = if [hd(V2)] =:= "—" -> " "; true -> "" end,
+      if Last =:= 32 -> ["\n", SP, "  ", B, V2, " "];
+        [Last] =:= "—" -> [B, V2];
+        true -> ["\n", SP, "  ", B, V2]
+      end
+  end.
+
+txtp([], _, _, Acc) -> lists:reverse(Acc);
+txtp([V|T], SP, Type, Acc) ->
+  Z = txtp(V, SP, Type),
+  txtp(T, SP, Type, [Z|Acc]).
+
+
+check([#xmlElement{name=h3,content=C}|_], false) -> check(C, {true, h3});
+check([#xmlElement{name=p,content=C}|_], false) -> check(C, {true, p});
+check([#xmlText{value=V}|MoreNodes], false) ->
+  G = trim2(V) == "",
+  if G -> check(MoreNodes, false); true -> {true, other} end;
+check([], {true, h3}) -> {true, other};
+check([#xmlText{value=_V} | _], {true, h3}) -> {true, usual};
+check([_|_], {true, h3}) -> {true, other};
+check([#xmlText{value=_V} | MoreNodes], {true, p}) -> hasa(MoreNodes);
+check(_, _) -> {true, other}.
+
+hasa([#xmlElement{name=a}|_]) -> {true, last};
+hasa([#xmlElement{name=b,content=[#xmlElement{name=a}|_]}|_]) -> {true, last};
+hasa([#xmlElement{name=b,content=[#xmlText{value=_V}, #xmlElement{name=a}|_]}|_])->{true, last};
+hasa(_) -> {true, other}.
 
 
 head([], A) -> A;
